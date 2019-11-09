c891a6cf029e7a29c7ff982904a4a31af1ef50db
Katerina Intzevidou <intz.katerina@gmail.com>
2019-11-09 17:50:05 +0100

    Change bubble sort to stop when there are no swaps left

--- src/sorting_algos/BubbleSort.java
+++ src/sorting_algos/BubbleSort.java
@@ -6,21 +6,25 @@ public class BubbleSort {
     public static void main(String[] args) {
 //        int[] array = {8, 1, 5, 2, 3, 6, 1};
         int arraySize = 50;
-        int[] array = BasicArrayOperations.createRandomArray(50);
+        int[] array = BasicArrayOperations.createRandomArray(arraySize);
 
         bubbleSort(array);
         BasicArrayOperations.printArray(array);
     }
 
-    //TODO Break when no more swaps left
-    static void bubbleSort(int[] array) {
+    private static void bubbleSort(int[] array) {
         int arraySize = array.length;
+        boolean swapped;
         for (int j = 0 ; j < arraySize ; j++) {
+            swapped = false;
             for (int i = 0; i < arraySize - 1; i++) {
                 if (array[i + 1] < array[i]) {
+                    swapped = true;
                     BasicArrayOperations.swap(array, i, i + 1);
                 }
             }
+            if (!swapped)
+                break;
         }
     }
 }
