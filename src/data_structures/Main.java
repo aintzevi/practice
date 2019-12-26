15e4ab89714cb756aeaece482cb9e56eb2801ac2
aintzevi <intz.katerina@gmail.com>
2019-12-26 16:50:20 +0100

    Add main method

---
+++ src/data_structures/Main.java
@@ -0,0 +1,18 @@
+package data_structures;
+
+public class Main {
+    public static void main(String[] args) {
+        QueueUsingLinkedList queue = new QueueUsingLinkedList();
+
+        for (int i = 1; i <= 4; i++) {
+            Node newNode = new Node(i);
+            queue.add(newNode);
+        }
+        queue.printAll();
+        System.out.println("Delete one by one");
+        while(!queue.isEmpty()) {
+            queue.remove();
+            queue.printAll();
+        }
+    }
+}
