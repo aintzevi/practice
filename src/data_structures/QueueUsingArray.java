501951794cd57a9da866978670ef96972378a40d
aintzevi <intz.katerina@gmail.com>
2019-12-27 14:06:16 +0100

    Make Array Queue generic

--- src/data_structures/QueueUsingArray.java
+++ src/data_structures/QueueUsingArray.java
@@ -4,8 +4,8 @@ package data_structures;
  * Queue implementation using array
  */
 
-public class QueueUsingArray {
-    private int[] queue;
+public class QueueUsingArray<E> {
+    private Object[] queue;
     private int frontIndex;
     private int rearIndex;
     private int size;
@@ -13,8 +13,8 @@ public class QueueUsingArray {
     /**
      * Initializes array of size 10
      */
-    QueueUsingArray () {
-        queue = new int[10];
+    QueueUsingArray() {
+        queue = new Object[10];
         frontIndex = rearIndex = 0;
         size = 0;
     }
@@ -22,9 +22,9 @@ public class QueueUsingArray {
     /**
      * Initializes array of desired size
      */
-    QueueUsingArray (int size) {
+    QueueUsingArray(int size) {
         this.size = size;
-        queue = new int[size];
+        queue = new Object[size];
         frontIndex = rearIndex = 0;
     }
 
@@ -37,25 +37,24 @@ public class QueueUsingArray {
 
     /**
      * Adds number in the queue, given that the queue is not full
+     *
      * @param value number to be added in the queue
      */
-    public void add(int value) {
-        if(rearIndex < size - 1){
+    public void add(E value) {
+        if (rearIndex < size - 1) {
             queue[rearIndex] = value;
             rearIndex++;
-        }
-        else {
+        } else {
             // shift values backward and update front and rear index
-            if(frontIndex > 0) {
+            if (frontIndex > 0) {
                 int numberOfElements = rearIndex - frontIndex + 1;
-                for (int i = 0 ; i < numberOfElements ; i++) {
-                    System.arraycopy(queue, frontIndex + i, queue, i, numberOfElements );
+                for (int i = 0; i < numberOfElements; i++) {
+                    System.arraycopy(queue, frontIndex + i, queue, i, numberOfElements);
                 }
 
                 frontIndex = 0;
                 rearIndex = numberOfElements;
-            }
-            else {
+            } else {
                 System.out.println("No space left for new values!");
                 // TODO: Update to extending array
             }
@@ -64,11 +63,12 @@ public class QueueUsingArray {
 
     /**
      * Remove top item from the queue
+     *
      * @return the top number of the queue
      */
-    public int remove() {
-        int currentFront = queue[frontIndex];
-        if(frontIndex < size - 1 && frontIndex < rearIndex)
+    public E remove() {
+        E currentFront = (E) queue[frontIndex];
+        if (frontIndex < size - 1 && frontIndex < rearIndex)
             frontIndex++;
         else {
             System.out.println("Queue is empty");
@@ -78,7 +78,6 @@ public class QueueUsingArray {
     }
 
     /**
-     *
      * @return true if the queue is empty, false otherwise
      */
     public boolean isEmpty() {
@@ -88,8 +87,8 @@ public class QueueUsingArray {
     /**
      * Prints the items inside the queue in a line
      */
-    public void printElements () {
-        for (int i = frontIndex ; i < rearIndex; i++) {
+    public void printElements() {
+        for (int i = frontIndex; i < rearIndex; i++) {
             System.out.print(queue[i] + " ");
         }
         System.out.println();
